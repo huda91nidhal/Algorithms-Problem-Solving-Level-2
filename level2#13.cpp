0220b92a8499d9ceb4f779a87174c35532d0fd77
huda91nidhal <hudaalshomar@gmail.com>
2026-05-15 14:50:40 +0300

    Add files via upload

---
+++ level2#13.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+int readpositivenumber(string message)
+{
+    int number;
+    do
+    {
+        cout << message << endl;
+        cin >> number;
+    } while (number <= 0);
+    return number;
+}
+void printnumberpattern(int number)
+{
+    cout << endl;
+
+    for (int i = 1; i <= number; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << i;
+        }
+        cout << endl;
+
+    }
+}
+int main()
+{
+    printnumberpattern(readpositivenumber("Please enter a positive number "));
+    return 0;
+}
