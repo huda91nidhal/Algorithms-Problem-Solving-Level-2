f4a444548710d25ce1b9f1e0ab7a3924b29474e3
huda91nidhal <hudaalshomar@gmail.com>
2026-05-15 15:34:50 +0300

    Add files via upload

---
+++ level2#26.cpp
@@ -0,0 +1,46 @@
+#include <iostream>   
+using namespace std;
+int RandomNumber(int From, int To)
+{
+    int randNum = rand() % (To - From + 1) + From;
+    return randNum;
+}
+void FillArrayWithRandomNumbers(int arr[100], int& arrLength)
+{
+    cout << "\nEnter number of elements:\n";
+    cin >> arrLength;
+
+
+    for (int i = 0; i < arrLength; i++)
+        arr[i] = RandomNumber(1, 100);
+}
+void PrintArray(int arr[100], int arrLength)
+{
+
+    for (int i = 0; i < arrLength; i++)
+        cout << arr[i] << " ";
+    cout << "\n";
+}
+int SumArray(int arr[100], int arrLength)
+{
+    int sum = 0;
+    for (int i = 0; i < arrLength; i++)
+    {
+        sum += arr[i];
+    }
+    return sum;
+}
+int main()
+{
+    srand((unsigned)time(NULL));
+
+    int arr[100];
+    int arrLength;
+    FillArrayWithRandomNumbers(arr, arrLength);
+    cout << "\n Array Elements: ";
+    PrintArray(arr, arrLength);
+    cout << "\n Sum of all number is : ";
+    cout << SumArray(arr, arrLength) << endl;
+
+    return 0;
+}
