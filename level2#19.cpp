19e4658abed9a0b0ff676f3c247c0881384dcbdb
huda91nidhal <hudaalshomar@gmail.com>
2026-05-15 14:57:38 +0300

    Add files via upload

---
+++ level2#19.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include<cstdlib>
+using namespace std;
+int RandomNumber(int From, int To)
+{
+	int randomnum = rand() % (To - From + 1) + From;
+	return randomnum;
+}
+int main()
+{
+	srand((unsigned)time(NULL));
+
+	cout << RandomNumber(1, 10) << endl;
+	cout << RandomNumber(1, 10) << endl;
+	cout << RandomNumber(1, 10) << endl;
+	return 0;
+}
